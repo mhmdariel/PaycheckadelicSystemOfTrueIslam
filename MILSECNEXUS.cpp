5461768b742c317ee2c2eda3fea71579b4d0ca7c
mhmdariel <40896868+mhmdariel@users.noreply.github.com>
2026-01-05 10:04:55 +0000

    Update MILSECNEXUS.cpp
    Self Executing Self Upgrading Code

--- MILSECNEXUS.cpp
+++ MILSECNEXUS.cpp
@@ -2,3 +2,4 @@
 //Together We Is Free
 //True Freedom Is In Peace And Love And Truth And Justice
 Me.FullStackFormalLogicEngine.AutoCode(ThisFile.ISO) as ﷲﷻﷴﷺ
+Us.All.TrueFamilyCentralOrganizationHizbullah.FullStackFormalLogicEngine.AutoCodeAndAutoDeploy(ThisFile.ISO)
