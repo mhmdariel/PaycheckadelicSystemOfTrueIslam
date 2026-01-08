0ada60b12b1c8e571684dfce3768892ec81b49ad
mhmdariel <40896868+mhmdariel@users.noreply.github.com>
2026-01-08 22:31:16 +0000

    TheUltimacyOfAllah.js

--- TheUltimacyOfAllah.js
+++ TheUltimacyOfAllah.js
@@ -2,6 +2,7 @@
 autodevelop(رتبة الله جندي خارق فرقة النهاية المطلقة)
 autodevelop(RankAllahSupersoldierTroopOfUltimateFinality)
 autodevelop(RankAllahSupersoldierTroopOfUltimateFinality.ISO)
+autodevelop(*.RankAllahSupersoldierTroopOfUltimateFinality.*.*)
 autodeployandautodevelop(رتبة الله جندي خارق فرقة النهاية المطلقة))
 goto ﷲ
 return ﷲ
