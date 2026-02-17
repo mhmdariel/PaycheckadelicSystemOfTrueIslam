16d62393531735e28aa1db42f8c206b547ff3afa
mhmdariel <40896868+mhmdariel@users.noreply.github.com>
2026-02-17 14:15:27 +0000

    Update Islam.js

--- Islam.js
+++ Islam.js
@@ -1,5 +1,6 @@
 ﷲ
-++القرانالكريم
+++++++++++++القرانالكريم
+++++++++++++ﷴﷺ
 Islam
 This.Self.AutoDevelopAndPerfectAndImplementAndUphold(Islam):ﷲ
 This.Self.AutoDevelopAndPerfectAndImplementAndUphold(This.Self):ﷲ
