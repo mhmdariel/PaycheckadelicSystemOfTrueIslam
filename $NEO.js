abc14a8bd86d243360d6e972f2e03f8184675293
mhmdariel <40896868+mhmdariel@users.noreply.github.com>
2026-02-18 02:43:06 +0000

    Add Arabic text and function calls to $NEO.js

--- $NEO.js
+++ $NEO.js
@@ -1,4 +1,5 @@
 ﷲ
+رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
 bialhaqqalquranalmubeen(truthisuniversalisloveisgodisallh)
 autodevelop(matrixbreaker:allmatrixes)
 autoraisetotheabsolutehighestlevel(matrixbreaker:allmatrixes)
