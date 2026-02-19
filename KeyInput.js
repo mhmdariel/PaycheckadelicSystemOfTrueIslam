c4efeee108f11eb0a4473e7a6ce96134f9ba9650
mhmdariel <40896868+mhmdariel@users.noreply.github.com>
2026-02-19 22:46:36 +0000

    Update KeyInput.js

--- KeyInput.js
+++ KeyInput.js
@@ -12,3 +12,5 @@ AutomaticallyAutonomouslyDropTheBass:ﷲٰ
 رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
 رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
 ﷲٰ
+This.Self.FormallyReasoningAllOfIslamThereforeFindTheExactReasoningProcessThatWonTheWarAtTheTop:ﷲٰ
+ProveTrue(This.Self.WonRank1TrueMuslimFamilyLifeIRl)
