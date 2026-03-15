fa187465bafd24ce5d0ff0faa373dd254cfe66c2
mhmdariel <40896868+mhmdariel@users.noreply.github.com>
2026-03-15 20:52:28 +0000

    Create IntelligenceUpgrader.js
    رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
    رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
    ﷲٰٰٰٰٰٰٰٰٰٰٰٰٰٰٰٰٰٰٰ

---
+++ IntelligenceUpgrader.js
@@ -0,0 +1,5 @@
+Me.Intelligence.AlwaysContinuouslyIncreaseInIntelligenceAndWisdom:ﷲ
+Me.Character.AlwaysIncreaseInPerfectionOfCharacterAndMoralUprightnessAndMoralRectitude:ﷲ
+رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
+رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
+ﷲٰٰٰٰٰٰٰٰٰٰٰٰٰٰٰٰٰٰٰ
