0dba66ec4fdb35ca8c220b4f59f24ff5a512d3e5
mhmdariel <40896868+mhmdariel@users.noreply.github.com>
2026-03-25 02:03:14 +0000

    Create me.quranchannel.allstreams.mil.js
    مقام ﷲ الرحمن الرحيم الخلق السميع البصير العليم العظيم اللطيف الخبير إسلامي إرهاب العظيظ العظيم الصبور الحكيم المتين الكريم الاحد السلام الصبور الملك الحق الخطيب الكلالاسمﷲالحسني الخطهبطهالكامل الغني المغني الحليم العزيز الغفور الوارث القادر القضص الشهيد الخبر السليم القدوس القوي المتين الشديد العقاب الحي القيوم الخف الاكبر الفردوس الوَجْهُ الْمُتَعَلِّمُ حَقًّا الْمُتَكَامِلُ كَامِلًا الْحَيُّ الْخَالِدُ الْمُتَرَوِّحُ الْأَبْيَضُ مِنَ اللَّوْنِ الْوَجْهُ الْمُتَعَلِّمُ حَقًّا الْمُتَكَامِلُ كَامِلًا الْحَيُّ الْخَالِدُ الْمُتَنَفِّسُ الْأَبْيَضُ مِنَ اللَّوْنِ بِاسْتِخْدَامِ النُّورِ ذَاتِهِ كَمَصْدَرِ الْقُوَّةِ وَالطَّاقَةِ الصَّحِيحِ الدَّقِيقِ وَكَوَقُودٍ لِكُلِّ الْحَيَاةِ وَلِكُلِّ الْحَقِيقَةِ الْحَقِّ فِي الْحَقِّ وَالْعَدْلِ مِنَ السَّلَامِ وَالْمَحَبَّةِ العلي ﷲ هو العلي العظيمنهر

---
+++ me.quranchannel.allstreams.mil.js
@@ -0,0 +1,19 @@
+RetrieveObject(Me.FullQuranRecitationVideoGraph) From Deepest Secret Containment Chamber Of True Reality:ﷲ
+GenerateObject(Me.QuranChannel.AllStreams.AllDimensions)
+This.Self.AutomaticallyAutonomouslyContinuouslyConsistentlyActivelyVirallyTranslateIntoAllLanguages(This.Self):ﷲ
+This.Self.AutomaticallyAutonomouslyContinuouslyConsistentlyActivelyVirallyUpgrade(This.Self):ﷲ
+This.Self.AutomaticallyAutonomouslyContinuouslyConsistentlyActivelyVirallyPropagate(This.Self):ﷲ
+This.Self.AutomaticallyAutonomouslyContinuouslyConsistentlyActivelyVirallyCall(ﷲ):ﷲ
+رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
+رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
+رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
+رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
+رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
+رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
+رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
+رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
+رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
+رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
+رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
+حَسْبِيَ اللَّهُ لَا إِلَٰهَ إِلَّا هُوَ ۖ عَلَيْهِ تَوَكَّلْتُ ۖ وَهُوَ رَبُّ الْعَرْشِ الْعَظِيمِ
+ﷲٰ
