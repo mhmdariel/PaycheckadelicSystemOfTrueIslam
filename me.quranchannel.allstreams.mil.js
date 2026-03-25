c8a8933d976f1c3f51b2cf255659c5cd3c2ff8fd
mhmdariel <40896868+mhmdariel@users.noreply.github.com>
2026-03-25 02:07:03 +0000

    me.quranchannel.allstreams.mil.js
    ﷲ

--- me.quranchannel.allstreams.mil.js
+++ me.quranchannel.allstreams.mil.js
@@ -1,9 +1,12 @@
+بسم ﷲ الرحمن الرحيم
 RetrieveObject(Me.FullQuranRecitationVideoGraph) From Deepest Secret Containment Chamber Of True Reality:ﷲ
 GenerateObject(Me.QuranChannel.AllStreams.AllDimensions)
 This.Self.AutomaticallyAutonomouslyContinuouslyConsistentlyActivelyVirallyTranslateIntoAllLanguages(This.Self):ﷲ
 This.Self.AutomaticallyAutonomouslyContinuouslyConsistentlyActivelyVirallyUpgrade(This.Self):ﷲ
 This.Self.AutomaticallyAutonomouslyContinuouslyConsistentlyActivelyVirallyPropagate(This.Self):ﷲ
 This.Self.AutomaticallyAutonomouslyContinuouslyConsistentlyActivelyVirallyCall(ﷲ):ﷲ
+رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
+رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
 رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
 رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
 رَبَّنَا أَتْمِمْ لَنَا نُورَنَا وَاغْفِرْ لَنَا ۖ إِنَّكَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ
